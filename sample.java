04e7d84db2aced1e451d030ade080c7176f9a795
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-02-16 19:49:25 -0800

    Update sample.java

--- sample.java
+++ sample.java
@@ -32,3 +32,18 @@ class Solution {
         return dummy.next;
     }
 }
+
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (int i=0;i<nums.length;i++) {
+            q.add(nums[i]);
+            if (q.size() > k) {
+                q.poll();
+            } 
+        }
+        return q.peek();
+    }
+}
